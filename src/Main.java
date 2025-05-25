802d517715a53d4601a0c40c7656dbe201aecc6e
berfinesenn <esenbef@gmail.com>
2025-05-25 20:02:30 +0300

    Java da deÄgiskenler ve veri tipleri

--- src/Main.java
+++ src/Main.java
@@ -1,4 +1,19 @@
 public class Main {
+    public static void main(String[] args) {
+        int yas = 24;
+        double maas = 22500.75;
+        float sicaklik = 35.6f;
+        boolean aktifMi = true;
+        char cinsiyet = 'K';
+        String ad = "Berfin";
+        final double PI = 3.14159;
 
-
+        System.out.println("Yaş: " + yas);
+        System.out.println("Maaş: " + maas);
+        System.out.println("Sıcaklık: " + sicaklik);
+        System.out.println("Aktif mi?: " + aktifMi);
+        System.out.println("Cinsiyet: " + cinsiyet);
+        System.out.println("Ad: " + ad);
+        System.out.println("Pi sayısı: " + PI);
+    }
 }
